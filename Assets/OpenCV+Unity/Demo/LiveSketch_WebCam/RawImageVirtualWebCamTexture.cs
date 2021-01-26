6dd03d832a445a198876cee232c6461492b3b7b9
Just-D-A <32079550+Just-D-A@users.noreply.github.com>
2021-01-27 02:54:34 +0300

    try to add butterfly to canvas

---
+++ Assets/OpenCV+Unity/Demo/LiveSketch_WebCam/RawImageVirtualWebCamTexture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageVirtualWebCamTexture : MonoBehaviour
+{
+    private RawImage rawimage;
+
+    void Start()
+    {
+        GameObject obj = GameObject.Find("RawImageTransform");
+        rawimage = obj.GetComponent<RawImage>();
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (source != null)
+        {
+            rawimage.texture = source;
+        }
+        Graphics.Blit(source, destination);
+    }
+}
